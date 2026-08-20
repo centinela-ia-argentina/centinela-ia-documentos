1b3fec2324dd638f0efaea8302f58ea1e7de6a02
Tobias Perez <tobiasperez188@gmail.com>
2026-08-19 23:26:13 -0300

    chore-remove-temporary-github-log-files

--- scratch_fetch.js
+++
@@ -1,6 +0,0 @@
-const https = require('https');
-https.get('https://api.github.com/repos/centinela-ia-argentina/centinela-ia-documentos/actions/runs/32319820834/jobs', {headers: {'User-Agent': 'Node.js'}}, (res) => {
-  let data = '';
-  res.on('data', d => data += d);
-  res.on('end', () => console.log(data));
-});
